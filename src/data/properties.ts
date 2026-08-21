92fa9ff53d9b46e0c0e786ba6f04891bf646d3f8
faiqxz <faiqsc@gmail.com>
2026-08-21 08:29:42 +0700

    Update Kos Nyaman 2 Tipe A status to rented

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -79,10 +79,10 @@ export const properties: Property[] = [
       { label: 'AC', value: 'Tiap Kamar' },
       { label: 'Tagihan', value: 'Bulanan' },
     ],
-    status: 'available',
+    status: 'rented',
     mapsUrl: 'https://maps.app.goo.gl/7WT1v7Fidvf7xHrb9',
     coords: [-2.9836, 104.7349],
-    badge: 'Tersedia',
+    badge: 'Sudah Tersewa',
   },
   {
     id: 'kos-nyaman-tipe-b',
